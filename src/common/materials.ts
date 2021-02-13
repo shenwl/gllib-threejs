77eba28f05cb6e0905424976f7c31450269ce0e9
shenwl <shenweilun96@gmail.com>
2021-02-13 22:11:35 +0800

    材质创建函数

--- src/common/materials.ts
+++ src/common/materials.ts
@@ -1,2 +1,10 @@
 import * as Three from 'three';
+import _uniqueId from 'lodash/uniqueId';
 
+export const createMaterial = (type: MaterialType, opts: any) => {
+  return new Three[type]({
+    name: _uniqueId('material'),
+    ...opts,
+    color: opts.color || 0x222222,
+  });
+}
--- typings.d.ts
+++ typings.d.ts
@@ -2,3 +2,6 @@ declare module '*.css';
 declare module '*.less';
 
 type Xyz = { x: number, y: number, z: number };
+
+type MaterialType = 'MeshBasicMaterial' | 'MeshNormalMaterial' | 'MeshStandardMaterial' |
+  'MeshPhongMaterial' | 'MeshLambertMaterial' | 'MeshToonMaterial';  
